8f52bde21638f1ce0c34b36e8574a98b53e915f6
Emily Corso <ekcorso@gmail.com>
2021-11-30 17:48:28 -0800

    Add stubs for SettingsFormVC and API class

---
+++ BoxingScheduler/Networking/AcuityAPI.swift
@@ -0,0 +1,13 @@
+//
+//  AcuityAPI.swift
+//  BoxingScheduler
+//
+//  Created by Emily Corso on 11/30/21.
+//
+
+import Foundation
+import SwiftSoup
+
+class AcuityAPI {
+    
+}
---
+++ BoxingScheduler/Views/SettingsFormViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SettingsFormViewController.swift
+//  BoxingScheduler
+//
+//  Created by Emily Corso on 11/30/21.
+//
+
+import UIKit
+
+class SettingsFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
