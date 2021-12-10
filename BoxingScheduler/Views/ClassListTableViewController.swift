1ab3b6f96474962e0d05a4161e06e19ad97dcbc4
Emily Corso <ekcorso@gmail.com>
2021-12-09 20:28:43 -0800

    Set up tableView and implement data source
    This is an ugly attempt to use the array of Dates returned by the
    parsing func as the data source for the tableView. Still working on
    moving this functionality elsewhere, but this is a working version of
    it.

--- BoxingScheduler/Views/ClassListTableViewController.swift
+++ BoxingScheduler/Views/ClassListTableViewController.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
+import SwiftSoup
 
 class ClassListTableViewController: UITableViewController {
-    let classList = ["Cardio Boxing", "Boxing Team Practice", "Boxing Skills", "Footwork FUNdamentals"]
+    var dateList = [Date]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.title = "Classes Available"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        getUrlContent()
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -29,14 +32,15 @@ class ClassListTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return classList.count
+        return dateList.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        let boxingClass = classList[indexPath.row]
-        cell.textLabel?.text = boxingClass
+        let classDate = dateList[indexPath.row]
+        cell.textLabel?.text = classDate.exactDate!
+        cell.accessoryType = .disclosureIndicator
         
         return cell
     }
@@ -85,5 +89,90 @@ class ClassListTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func getUrlContent() -> URLSessionDataTask? {
+        guard let url = URL(string: "https://app.squarespacescheduling.com/schedule.php?action=showCalendar&fulldate=1&owner=19967298&template=class"), let payload = "type=&calendar=&skip=true&options%5Bqty%5D=1&options%5BnumDays%5D=5&ignoreAppointment=&appointmentType=&calendarID=".data(using: .utf8) else {
+            return nil
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/x-www-form-urlencoded; charset=UTF-8", forHTTPHeaderField: "content-type")
+        request.httpBody = payload
+
+        let dataTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard error == nil else {
+                print(error!.localizedDescription)
+                return
+            }
+            guard let data = data else {
+                print("Empty data")
+                return
+            }
+            if let str = String(data: data, encoding: .utf8) {
+                let stringData = self.parseHtml(fromString: str)
+                //self.createDateList(from: stringData!)
+                self.dateList = self.createDateList(from: stringData!)
+            }
+        }
+
+        dataTask.resume()
+
+        return dataTask
+    }
+
+    func parseHtml(fromString: String) -> Document? {
+        do {
+            let html: String = fromString
+            let doc: Document = try SwiftSoup.parse(html)
+            return doc
+        } catch Exception.Error(_, let message) {
+            print(message)
+            return nil
+        } catch {
+            print("error")
+            return nil
+        }
+    }
+
+    func createDateList(from doc: Document) -> [Date] {
+        var dateArray = [Date]()
+        guard let elements = try? doc.select("tr") else {
+            print("select for tr failed")
+            return dateArray
+        }
+
+        for (index, item) in elements.enumerated() {
+            let date = Date()
+            do {
+                if item.hasClass("class-date-row") {
+                    let exactDate = try item.select(".class-date-row").text()
+                    date.exactDate = exactDate
+                    dateArray.append(date)
+                } else if try! item.className().contains("class-row-xs") {
+                    let name = try item.text()
+                    let spotsAvailable = try elements[index + 1].text()
+                    let boxingClass = MbaClass(name: name, spotsAvailable: spotsAvailable)
+                    if let previousDate = dateArray.last {
+                        previousDate.classes.append(boxingClass)
+                    }
+                }
+            } catch Exception.Error(_, let message) {
+                print(message)
+            } catch {
+                print("other exception")
+            }
+        }
+        
+//        for date in dateArray {
+//            print("Classes on date \(date.exactDate): \(date.classes.count)")
+//        }
+        
+        DispatchQueue.main.async {
+            self.dateList = dateArray
+            self.tableView.reloadData()
+        }
+        return dateArray
+    }
 
 }
