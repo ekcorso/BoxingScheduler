2f509c38c8683c2027b3fdb1502415a70377a97a
Emily Corso <ekcorso@gmail.com>
2021-12-14 18:38:33 -0800

    Build out custom tableView cell for DateListVC

---
+++ BoxingScheduler/Views/MbaClassTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MbaClassTableViewCell.swift
+//  BoxingScheduler
+//
+//  Created by Emily Corso on 12/14/21.
+//
+
+import UIKit
+
+class MbaClassTableViewCell: UITableViewCell {
+    static let identifier = "CustomClassCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        self.accessoryType = .disclosureIndicator
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setCellText(mbaClass: MbaClass) {
+        textLabel?.text = "\(mbaClass.name)"
+        detailTextLabel?.text = "(Spots available: \(mbaClass.spotsAvailable))"
+    }
+}
