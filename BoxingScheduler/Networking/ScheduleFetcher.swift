1f3c8c1318ce6c40a915126710fbc43d5101ec61
Emily Corso <ekcorso@gmail.com>
2021-12-14 17:20:37 -0800

    Add alternative networking class, ScheduleFetcher

---
+++ BoxingScheduler/Networking/ScheduleFetcher.swift
@@ -0,0 +1,113 @@
+//
+//  ScheduleFetcher.swift
+//  BoxingScheduler
+//
+//  Created by Emily Corso on 12/14/21.
+//
+
+import Foundation
+import SwiftSoup
+import Combine
+
+class ScheduleFetcher {
+    var dateList: [Date]? {
+        didSet {
+            if let dateList = dateList {
+                print("DateList updated")
+            }
+        }
+    }
+    
+    init() {
+        DispatchQueue.main.async {
+            self.getUrlContent()
+        }
+    }
+    
+    func getUrlContent() -> URLSessionDataTask? {
+        guard let url = URL(string: "https://app.squarespacescheduling.com/schedule.php?action=showCalendar&fulldate=1&owner=19967298&template=class"), let payload = "type=&calendar=&skip=true&options%5Bqty%5D=1&options%5BnumDays%5D=5&ignoreAppointment=&appointmentType=&calendarID=".data(using: .utf8) else {
+            return nil
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/x-www-form-urlencoded; charset=UTF-8", forHTTPHeaderField: "content-type")
+        request.httpBody = payload
+
+        let dataTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard error == nil else {
+                print(error!.localizedDescription)
+                return
+            }
+            guard let data = data else {
+                print("Empty data")
+                return
+            }
+            if let str = String(data: data, encoding: .utf8) {
+                    let stringData = self.parseHtml(fromString: str)
+                DispatchQueue.main.async {
+                    self.createDateList(from: stringData!)
+//                    self.dateList = self.createDateList(from: stringData!)
+                }
+            }
+        }
+
+        dataTask.resume()
+
+        return dataTask
+    }
+
+    func parseHtml(fromString: String) -> Document? {
+        do {
+            let html: String = fromString
+            let doc: Document = try SwiftSoup.parse(html)
+            return doc
+        } catch Exception.Error(_, let message) {
+            print(message)
+            return nil
+        } catch {
+            print("error")
+            return nil
+        }
+    }
+
+    func createDateList(from doc: Document) -> [Date] {
+        var dateArray = [Date]()
+        guard let elements = try? doc.select("tr") else {
+            print("select for tr failed")
+            return dateArray
+        }
+
+        for (index, item) in elements.enumerated() {
+            let date = Date()
+            do {
+                if item.hasClass("class-date-row") {
+                    let exactDate = try item.select(".class-date-row").text()
+                    date.exactDate = exactDate
+                    dateArray.append(date)
+                } else if try! item.className().contains("class-row-xs") {
+                    let name = try item.text()
+                    let spotsAvailable = try elements[index + 1].text()
+                    let boxingClass = MbaClass(name: name, spotsAvailable: spotsAvailable)
+                    if let previousDate = dateArray.last {
+                        previousDate.classes.append(boxingClass)
+                    }
+                }
+            } catch Exception.Error(_, let message) {
+                print(message)
+            } catch {
+                print("other exception")
+            }
+        }
+        
+        for date in dateArray {
+            print("Classes on date \(date.exactDate!): \(date.classes.count)")
+        }
+        
+        DispatchQueue.main.async {
+            self.dateList = dateArray
+        }
+        
+        return dateArray
+    }
+}
