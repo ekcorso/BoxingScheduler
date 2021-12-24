8df5c48646d25c0b669977ed5eec3b93fa301fe1
Emily Corso <ekcorso@gmail.com>
2021-12-23 18:46:34 -0800

    Draft NowAvailableTableView and add to tabBar
    This commit also adds a view for a custom tableViewCell.

---
+++ BoxingScheduler/Views/NowAvailableCell.swift
@@ -0,0 +1,26 @@
+//
+//  NowAvailableCell.swift
+//  BoxingScheduler
+//
+//  Created by Emily Corso on 12/23/21.
+//
+
+import Foundation
+import UIKit
+
+class NowAvailableCell: UITableViewCell {
+    static let identifier = "NowAvailableCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setCellText(mbaClass: MbaClass) {
+        textLabel?.text = "\(mbaClass.name)"
+        detailTextLabel?.text = "\(mbaClass.date.toString(format: DateHandler.dateOutputFormat))"
+    }
+}
--- BoxingScheduler/Views/TabBarViewController.swift
+++ BoxingScheduler/Views/TabBarViewController.swift
@@ -10,19 +10,23 @@ import UIKit
 class TabBarViewController: UITabBarController {
     let dateListViewController = DateListTableViewController()
     let watchedClassesViewController = WatchedClassesTableViewController()
+    let availableClassesViewController = NowAvailableTableViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         dateListViewController.tabBarItem = UITabBarItem(title: "Schedule", image: UIImage(systemName: "calendar"), tag: 1)
-        watchedClassesViewController.tabBarItem = UITabBarItem(title: "Watched Classes", image: UIImage(systemName: "list.star"), tag: 0)
+        watchedClassesViewController.tabBarItem = UITabBarItem(title: "Watched Classes", image: UIImage(systemName: "eye"), tag: 0)
         
-        viewControllers = [watchedClassesViewController, dateListViewController].map { UINavigationController(rootViewController: $0) }
+        availableClassesViewController.tabBarItem = UITabBarItem(title: "Available Classes", image: UIImage(systemName: "list.star"), tag: 2)
+        
+        viewControllers = [availableClassesViewController, watchedClassesViewController, dateListViewController].map { UINavigationController(rootViewController: $0) }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        // Is this updating the value of selectedClasses, or is that updated in the WatchedClasses vc itself?
         if let selectedClasses = DataStorage().retrieve() {
             watchedClassesViewController.selectedClasses = selectedClasses
         }
