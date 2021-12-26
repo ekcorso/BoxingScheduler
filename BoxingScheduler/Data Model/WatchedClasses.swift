9f5e8172aa9e1956d491f5358998840fc96d8c36
Emily Corso <ekcorso@gmail.com>
2021-12-25 20:40:38 -0800

    Re-add basic WC add/remove funcs

--- BoxingScheduler/Data Model/WatchedClasses.swift
+++ BoxingScheduler/Data Model/WatchedClasses.swift
@@ -12,5 +12,28 @@ class WatchedClasses {
     var past: [MbaClass]?
     var nowAvailable: [MbaClass]?
     
+    init() {
+        self.current = DataStorage().retrieve() ?? []
+    }
     
+    func addNewSelections(_ selections: [MbaClass]) {
+        if var currentSelections = self.current {
+            currentSelections += selections
+        } else {
+            current = selections
+        }
+    }
+
+    func removeSelections(_ selections: [MbaClass]) {
+        if let currentSelections = self.current {
+            var filtered = currentSelections
+            for item in selections {
+                if let index = filtered.firstIndex(of: item) {
+                    filtered.remove(at: index)
+                }
+            }
+            current = filtered
+        }
+    }
+
 }
