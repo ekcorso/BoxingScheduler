91ef8c91a1e31a4b3ab137cc7d94e02bbd221716
Emily Corso <ekcorso@gmail.com>
2021-12-25 20:37:48 -0800

    Re-draft WatchedClasses logic-- old logic is in WC2

--- BoxingScheduler/Data Model/WatchedClasses.swift
+++ BoxingScheduler/Data Model/WatchedClasses.swift
@@ -8,58 +8,9 @@
 import Foundation
 
 class WatchedClasses {
-    var current: [MbaClass]? {
-        didSet {
-            if let current = current {
-                do {
-                    try DataStorage().save(current)
-                } catch {
-                    print("saving current classes failed")
-                }
-            }
-        }
-    }
-    var past: [MbaClass]? {
-        guard let currentClasses = self.current else {
-            return nil
-        }
-        
-        let now = Date()
-        let past = currentClasses.filter() { $0.date < now }
-        removeSelections(past)
-        return past
-    }
+    var current: [MbaClass]?
+    var past: [MbaClass]?
+    var nowAvailable: [MbaClass]?
     
-    var nowAvailable: [MbaClass]? {
-        guard let currentClasses = self.current else {
-            return nil
-        }
-        let availableClasses = currentClasses.filter() { $0.spotsAvailable != 0 }
-        removeSelections(availableClasses)
-        return availableClasses
-    }
     
-    init() {
-        self.current = DataStorage().retrieve() ?? []
-    }
-    
-    func addNewSelections(_ selections: [MbaClass]) {
-        if var currentSelections = self.current {
-            currentSelections += selections
-        } else {
-            current = selections
-        }
-    }
-    
-    func removeSelections(_ selections: [MbaClass]) {
-        if let currentSelections = self.current {
-            var filtered = currentSelections
-            for item in selections {
-                if let index = filtered.firstIndex(of: item) {
-                    filtered.remove(at: index)
-                }
-            }
-            current = filtered
-        }
-    }
 }
