54302adbda55ffa16078a552240788d690ed5eb7
Emily Corso <ekcorso@gmail.com>
2021-12-27 08:26:26 -0800

    Build out logic for past/ current WC properties

--- BoxingScheduler/Data Model/WatchedClasses.swift
+++ BoxingScheduler/Data Model/WatchedClasses.swift
@@ -8,8 +8,34 @@
 import Foundation
 
 class WatchedClasses {
-    var current: [MbaClass]?
-    var past: [MbaClass]?
+    var current: [MbaClass]? {
+        didSet {
+            // Re-save the current list anytime it is updated (for instance by the methods that remove past/ available classes as those arrays are computed
+            if let current = current {
+                do {
+                    try DataStorage().save(current)
+                } catch {
+                    print("saving current classes failed")
+                }
+            }
+        }
+    }
+    
+    var past: [MbaClass]? {
+        guard let currentClasses = self.current else {
+            return nil
+        }
+        
+        let now = Date()
+        
+        // Initialize past selections array by filtering the current watched classes for dates that have past
+        let past = currentClasses.filter() { $0.date < now }
+        // removeSelections is commented out to keep from removing past classes so they can be potentially be used for testing
+        //        removeSelections(past)
+        return past
+        
+    }
+    
     var nowAvailable: [MbaClass]?
     
     init() {
@@ -35,5 +61,4 @@ class WatchedClasses {
             current = filtered
         }
     }
-
 }
