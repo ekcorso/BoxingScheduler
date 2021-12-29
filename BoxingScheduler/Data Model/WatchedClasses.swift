e042b8a921244de9a00b29c3a60d7392d7c62f5d
Emily Corso <ekcorso@gmail.com>
2021-12-28 20:25:04 -0800

    WIP: Major WatchedClasses refactor + add comments
    This major refactor removes past classes property, changes how classes
    are removed from current, spotsAvailable class property is updated, and how the list of all classes is fetched and used to calculate other properties. This refactor also changes the order in which things are updated so that once the list of allClasses is fetched the completion handler updates the other properties with that data. Marked WIP because this still needs work in order to be able to update the NATVC, currently the vc tries to access it before nowAvailable has been calculated.

--- BoxingScheduler/Data Model/WatchedClasses.swift
+++ BoxingScheduler/Data Model/WatchedClasses.swift
@@ -14,6 +14,7 @@ class WatchedClasses {
             if let current = current {
                 do {
                     try DataStorage().save(current)
+                    print("Array of current watched classes was updated")
                 } catch {
                     print("saving current classes failed")
                 }
@@ -21,25 +22,27 @@ class WatchedClasses {
         }
     }
     
-    var past: [MbaClass]? {
-        guard let currentClasses = self.current else {
-            return nil
-        }
-        
-        let now = Date()
-        
-        // Initialize past selections array by filtering the current watched classes for dates that have past
-        let past = currentClasses.filter() { $0.date < now }
-        // removeSelections is commented out to keep from removing past classes so they can be potentially be used for testing
-        //        removeSelections(past)
-        return past
-        
-    }
+    var allClassesFromAPI: [MbaClass]?
     
-    var nowAvailable: [MbaClass]?
+    // nowAvailable is initialized with dummy data to confirm everything else is working
+    var nowAvailable: [MbaClass]? = [MbaClass(name: "Team Practice Fridays (With Permission Only)", spotsAvailable: "2", date: "Friday, December 24, 2021")]
     
     init() {
         self.current = DataStorage().retrieve() ?? []
+        removePastClassesFromCurrent()
+        
+        getAllClassesFromAPI() { classList in
+            self.allClassesFromAPI = classList
+
+            self.updateSpotsAvailableForCurrent()
+            
+            // This save is necessary to pesist the changes from updating the spotsAvailable
+            do {
+                if let current = self.current {
+                    try? DataStorage().save(current)
+                }
+            }
+        }
     }
     
     func addNewSelections(_ selections: [MbaClass]) {
@@ -49,7 +52,8 @@ class WatchedClasses {
             current = selections
         }
     }
-
+    
+    //Removes classes whose dates have passed from the current array
     func removeSelections(_ selections: [MbaClass]) {
         if let currentSelections = self.current {
             var filtered = currentSelections
@@ -59,6 +63,68 @@ class WatchedClasses {
                 }
             }
             current = filtered
+            
+            do {
+                try DataStorage().save(filtered)
+            } catch {
+                print("Saving failed")
+            }
         }
     }
+    
+    //This func updates the class's allClassesFromAPI property when it is called in the initializer
+    private func getAllClassesFromAPI(completion: @escaping ([MbaClass]) -> ()) {
+        let fetcher = ScheduleFetcher()
+        var dateList = [ClassDate]()
+        var classList = [MbaClass]()
+        fetcher.getUrlContent() { dates in
+            dateList = dates
+            
+            for date in dateList {
+                for mbaClass in date.classes {
+                    classList.append(mbaClass)
+                }
+            }
+            completion(classList)
+        }
+    }
+    
+    //This func sorts updates the currently watched classes' spotsAvailable property when spots open up, then adds that class to the nowAvailable list
+    func updateSpotsAvailableForCurrent() {
+        guard let currentClasses = self.current else {
+            return
+        }
+
+        var availableClasses = [MbaClass]()
+        
+        // The following line is ONLY for testing, it creates at least one class that has spotsAvailable
+        currentClasses[1].spotsAvailable = 2
+        
+        for watchedClass in currentClasses {
+            for fetchedClass in allClassesFromAPI! {
+                //Check for a fetchedClass that matches a watchedClass AND has at least one spot available
+                if watchedClass.name == fetchedClass.name && watchedClass.date == fetchedClass.date && fetchedClass.spotsAvailable != 0 {
+                    watchedClass.spotsAvailable = fetchedClass.spotsAvailable
+                    availableClasses.append(watchedClass)
+                    print("\(fetchedClass.name) now has \(fetchedClass.spotsAvailable) spots available")
+                }
+            }
+        }
+        self.nowAvailable = availableClasses
+        print("NowAvailable was updated and now contains: \(self.nowAvailable?.count)")
+    }
+    
+    private func removePastClassesFromCurrent() {
+            guard let currentClasses = self.current else {
+                return
+            }
+            
+            let now = Date()
+            
+            // Initialize past selections array by filtering the current watched classes for dates that have passed
+            let past = currentClasses.filter() { $0.date < now }
+            // removeSelections is commented out to keep from removing past classes so they can be potentially be used for testing
+            removeSelections(past)
+    }
 }
+
