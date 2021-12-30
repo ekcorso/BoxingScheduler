0de35a74e00e41648dc71ff0b3e56d0836d5946d
Emily Corso <ekcorso@gmail.com>
2021-12-29 19:34:45 -0800

    Draft SettingsFormVC as a TableView

--- BoxingScheduler/Views/SettingsFormViewController.swift
+++ BoxingScheduler/Views/SettingsFormViewController.swift
@@ -7,23 +7,35 @@
 
 import UIKit
 
-class SettingsFormViewController: UIViewController {
+class SettingsFormViewController: UITableViewController {
+    let dataSource = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        title = "Settings"
 
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+    // MARK: - Table view data source
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
     }
-    */
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return dataSource.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
 }
