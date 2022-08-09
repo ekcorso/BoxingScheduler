cc902bc53c18e132b34ec02cd587c279006fdf64
Emily Corso <ekcorso@gmail.com>
2022-08-09 12:39:48 -0700

    Refactor fetching available classes

--- BoxingScheduler/Data Model/WatchedClasses.swift
+++ BoxingScheduler/Data Model/WatchedClasses.swift
@@ -78,32 +78,26 @@ class WatchedClasses {
     
     // This func sorts updates the currently watched classes' spotsAvailable property when spots open up then adds that class to the nowAvailable list and returns that list. Call this inside the completion handler for getAllClasses.
     func getNowAvailableClasses(from allClasses: [MbaClass]) -> [MbaClass] {
-        guard let currentClasses = self.current else {
+        guard let watched = self.current else {
             return [MbaClass]()
         }
         
-        var availableClasses = [MbaClass]()
+        let availableClasses = allClasses.filter() {
+            watched.contains($0) && $0.spotsAvailable > 0
+        }
         
-        for watchedClass in currentClasses {
-//            print(watchedClass.name)
-            for fetchedClass in allClasses{
-                // The following line is ONLY for testing: it creates at least one class that has spotsAvailable
-//                fetchedClass.spotsAvailable = 2
-                //Check for a fetchedClass that matches a watchedClass AND has at least one spot available
-                if watchedClass.name == fetchedClass.name && watchedClass.date == fetchedClass.date && fetchedClass.spotsAvailable != 0 {
-                    watchedClass.spotsAvailable = fetchedClass.spotsAvailable
-                    availableClasses.append(watchedClass)
-                    
-                    do {
-                        try DataStorage().save(currentClasses)
-                    } catch {
-                        print("Saving failed")
-                    }
-                }
+        // Update any classes that now have open spots as of this check
+        for mbaClass in availableClasses {
+            if let watchedClass = watched.first(where: {$0 == mbaClass }) {
+                watchedClass.spotsAvailable = mbaClass.spotsAvailable
             }
         }
         
-        return Array(Set(availableClasses))
+        self.current = watched
+        
+        let orderedArray = Array(Set(availableClasses))
+        
+        return orderedArray
     }
     
     private func removePastClassesFromCurrent() {
