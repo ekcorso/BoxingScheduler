2ca0fe06b59bb1ed11ddbde2972b52354a26b382
Emily Corso <ekcorso@gmail.com>
2022-10-25 17:26:43 -0700

    Enable saving/ retreiving nowAvailable list from files

--- BoxingScheduler/Data Model/DataStorage.swift
+++ BoxingScheduler/Data Model/DataStorage.swift
@@ -8,6 +8,36 @@
 import Foundation
 
 struct DataStorage {
+    // Let's re-write this with generics so that these methods don't duplicate so much code, or maybe switch to using CoreData, or use UserDefaults to persist data using keys/ values instead
+    
+    func saveNowAvailable(_ classList: [MbaClass]) throws {
+        let fileManager = FileManager()
+        let url = fileManager.getDocumentsDirectory().appendingPathComponent("nowAvailable.txt")
+
+        do {
+            let data = try NSKeyedArchiver.archivedData(withRootObject: classList, requiringSecureCoding: false)
+            try data.write(to: url)
+        } catch {
+            print("save failed")
+        }
+    }
+
+    func retrieveNowAvailable() -> [MbaClass]? {
+        let fileManager = FileManager()
+        let url = fileManager.getDocumentsDirectory().appendingPathComponent("nowAvailable.txt")
+
+        do {
+            let data = try Data(contentsOf: url)
+            if let decodedData = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? [MbaClass] {
+                return decodedData
+            }
+        } catch {
+            return nil
+        }
+        return nil
+    }
+    
+    
     func save(_ classList: [MbaClass]) throws {
         let fileManager = FileManager()
         let url = fileManager.getDocumentsDirectory().appendingPathComponent("classList.txt")
